38ce46e11f2a46d287def75069375ec9bceabd6c
Marc Gordon <mjg31337@gmail.com>
2018-04-12 11:40:40 -0400

    some helpfiul scripts im using

---
+++ scripts/scripts.sh
@@ -0,0 +1,15 @@
+
+
+
+ curl -X POST -H "Content-Type: application/json" -d'{"message": "foo"}' http://localhost:8181/messages
+
+
+ curl  http://localhost:8181/messages/2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
+
+
+
+ curl  http://10.235.129.84:8181/messages/2c26b46b68ffc68ff99b453c1d30413413422d706483bfa0f98a5e886266e7ae
+
+
+docker run -p 8181:8181 zzz
+ 

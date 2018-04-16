6074c1b4a4b3f6d5e3ab715af4c66a47948fd8a2
Marc Gordon <mjg31337@gmail.com>
2018-04-16 11:48:26 -0400

    more updates for ssl

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 This application is implemented in PHP 7.1 using Laravel 5.6, Redis, and DockerFile
 
-HTTPS isn't working yet, but will fix this soon
+Steps to run app
+
+Generate Certs
+Build Container
+Run Container
--- laravel/docker-ssl-cert.gen.sh
+++ laravel/docker-ssl-cert.gen.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 
-openssl genrsa -des3 -passout pass:x -out server.pass.key 2048
-openssl rsa -passin pass:x -in server.pass.key -out server.key
-rm server.pass.key
-openssl req -new -key server.key -out server.csr \
-    -subj "/C=UK/ST=Warwickshire/L=Leamington/O=OrgName/OU=IT Department/CN=example.com"
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+openssl genrsa -des3 -passout pass:x -out localhost.pass.key 2048
+openssl rsa -passin pass:x -in localhost.pass.key -out localhost.key
+rm localhost.pass.key
+openssl req -new -key localhost.key -out localhost.csr \
+    -subj "/C=US/ST=NY/L=Huntington/O=OrgName/OU=IT Department/CN=localhost"
+openssl x509 -req -days 365 -in localhost.csr -signkey localhost.key -out localhost.crt
